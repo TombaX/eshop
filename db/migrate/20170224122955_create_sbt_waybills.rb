f62853aae820adeea3acfac196cb88bd4c4a4367
lgor89 <igor.penkovsky@gmail.com>
2017-02-24 20:44:28 +0300

    improve table structure

--- db/migrate/20170224122955_create_sbt_waybills.rb
+++ db/migrate/20170224122955_create_sbt_waybills.rb
@@ -6,11 +6,10 @@ class CreateSbtWaybills < ActiveRecord::Migration
       t.integer         :sbt_order_id
       t.datetime        :date_issue
       t.integer         :sbt_waybill_items_count
-      t.decimal   :total_sum, precision: 10, scale: 2
-      t.decimal   :total_tax10_sum, precision: 10, scale: 2
-      t.decimal   :total_tax20_sum, precision: 10, scale: 2
+      t.decimal         :total_sum, precision: 10, scale: 2
+      t.decimal         :total_tax10_sum, precision: 10, scale: 2
+      t.decimal         :total_tax20_sum, precision: 10, scale: 2
       t.string          :auto_num
-
       t.timestamps null: false
     end
   end
